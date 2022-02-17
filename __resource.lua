a50f06b1d9821d0a034dd36402b6b2ef752e63a6
Kubca <36634763+Kubca@users.noreply.github.com>
2022-02-17 07:10:41 +0100

    Gangster Aim Animation
    Animation is easy to setup!
    
    There is a config file where you put your custom ESX and command name
    You can put more jobs to check and toggle it!
    
    Its not made by any trigger so cheater in job cant abuse it if he is not smart enough to just simply make the code...
    
    Animation should not be bugging it also contain an Aim check!

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+name 'Gangster Aim'
+author 'Kubca#1337'
+version 'V1'
+
+game 'gta5'
+
+client_script {'config.lua', 'gangaim-c.lua'}
+
+server_scripts {'config.lua', '@mysql-async/lib/MySQL.lua', 'gangaim-s.lua'}
