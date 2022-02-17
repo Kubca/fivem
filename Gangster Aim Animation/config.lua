4df1a43af36d434679a4985cc983dee0f24d8208
Kubca <36634763+Kubca@users.noreply.github.com>
2022-02-17 18:53:12 +0100

    Update config.lua

--- Gangster Aim Animation/config.lua
+++ Gangster Aim Animation/config.lua
@@ -6,3 +6,31 @@ Gang_Config.ESX_getSharedObject = "esx:getSharedObject" -- Change this if your s
 
 Gang_Config.Jobcheck = false -- Toggle job check so only people with job written bellow can use this animation  
 Gang_Config.Jobs = {"vagos", "bloods"} -- If you want to allow more jobs write them here
+
+Gang_Config.Weaponcheck = true -- Checking only for weapons that gangsters fr use and shoot with them like this!!!
+Gang_Config.Weapons = {
+    "weapon_pistol",
+    "weapon_pistol_mk2",
+    "weapon_combatpistol",
+    "weapon_appistol",
+    "weapon_stungun",
+    "weapon_pistol50",
+    "weapon_snspistol",
+    "weapon_snspistol_mk2",
+    "weapon_heavypistol",
+    "weapon_vintagepistol",
+    "weapon_flaregun",
+    "weapon_marksmanpistol",
+    "weapon_revolver",
+    "weapon_revolver_mk2",
+    "weapon_doubleaction",
+    "weapon_raypistol",
+    "weapon_ceramicpistol",
+    "weapon_navyrevolver",
+    "weapon_gadgetpistol",
+    "weapon_microsmg",
+    "weapon_minismg",
+    "weapon_machinepistol",
+    "weapon_compactrifle"
+}
+-- Allowed weapons to work with gangster animation
