e34b31caa476384101d21ac573b22c038fd081df
Kubca <36634763+Kubca@users.noreply.github.com>
2022-02-20 21:03:20 +0100

    Update map-fix.lua

--- Map Fix/map-fix.lua
+++ Map Fix/map-fix.lua
@@ -12,10 +12,11 @@ MapFix.F.Radar_Fix = function()
     MapFix.N.BigMap_Toggle = SetBigmapActive
     MapFix.N.Veh_Check = IsPedInAnyVehicle(PlayerPedId(), 0) 
     MapFix.N.BigMap_Check = IsBigmapActive() 
+    MapFix.N.FullMap_Check = IsBigmapFull()
     if Map_Config.Vehicle_Check then
         if MapFix.N.Veh_Check then 
             MapFix.N.Radar(true) 
-            if MapFix.N.BigMap_Check then 
+            if MapFix.N.BigMap_Check or MapFix.N.FullMap_Check then 
                 MapFix.N.BigMap_Toggle(false, false) 
             end 
         else 
@@ -23,7 +24,7 @@ MapFix.F.Radar_Fix = function()
         end 
     else
         MapFix.N.Radar(true) 
-        if MapFix.N.BigMap_Check then 
+        if MapFix.N.BigMap_Check or MapFix.N.FullMap_Check then 
             MapFix.N.BigMap_Toggle(false, false) 
         end 
     end
