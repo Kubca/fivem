df4b0136534ead346157ac646d52254a0dc76f52
Kubca <36634763+Kubca@users.noreply.github.com>
2022-02-21 01:36:30 +0100

    Update config.lua

--- Map Fix/config.lua
+++ Map Fix/config.lua
@@ -1,3 +1,4 @@
 Map_Config = {}
 
 Map_Config.Vehicle_Check = true -- Vehicle check for toggling map if you are in vehicle or not [[ Leave it to true if you dont want people to see map while not in car ]]
+Map_Config.RefreshTime = 1000 -- Refresh time for looping the function 
