1867d748109cfb0c5f5386e0348eabbfcf133a7a
Kubca <36634763+Kubca@users.noreply.github.com>
2022-02-21 02:09:29 +0100

    Update map-fix.lua

--- Map Fix/map-fix.lua
+++ Map Fix/map-fix.lua
@@ -12,15 +12,18 @@ MapFix.F.Radar_Fix = function()
     MapFix.N.Veh_Check = IsPedInAnyVehicle(MapFix.N.SelfPed, 0) 
     MapFix.N.BigMap_Check = IsBigmapActive() 
     if Map_Config.Vehicle_Check then
+        delay = 5
         if MapFix.N.Veh_Check then 
             DisplayRadar(true) 
             if MapFix.N.BigMap_Check then 
                 SetBigmapActive(false, false) 
             end 
         else 
+            delay = 1000
             DisplayRadar(false)
         end 
     else
+        delay = 5
         DisplayRadar(true) 
         if MapFix.N.BigMap_Check then 
             SetBigmapActive(false, false) 
@@ -29,7 +32,7 @@ MapFix.F.Radar_Fix = function()
 end 
 
 MapFix.F.Callback(function()
-    while MapFix.F.Enable do MapFix.F.Wait(Map_Config.RefreshTime)
+    while MapFix.F.Enable do MapFix.F.Wait(delay)
         MapFix.F.Radar_Fix()
     end
 end)
